05fd673f07a63ddbca4c50267fc7b9e7ec2b2e5b
TKZ <tkzellers@gmail.com>
2026-03-19 13:58:21 -0400

    contracts for stg, new tests, done with copilot

--- analyses/models_deprecated/dim_public__most_expensive_cards.sql
+++
@@ -1,8 +0,0 @@
--- select 
---     card_name,
---     set_name,
---     price,
---     rank() over (partition by set_name order by price desc) as rank_price
--- from --
--- where price is not null
--- order by rank_price, set_name
--- analyses/models_deprecated/dim_public__wordcount.sql
+++
@@ -1,6 +0,0 @@
--- select
--- card_name,
--- {{ word_count("card_text") }} as word_count,
--- card_text,
--- uri
--- from 
